f5613b18592ac7290dacb3711c5ce2725c3bc960
mahmoud0nasser <mhnasser00@gmail.com>
2025-10-09 11:39:19 +0300

    Integrate MongoDB connection and configuration in main and config

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -4,9 +4,13 @@ class Settings(BaseSettings):
     APP_NAME: str
     APP_VERSION: str
     OPENAI_API_KEY: str
+    
     FILE_ALLOWED_TYPES: list
     FILE_MAX_SIZE: int  # in MB
     FILE_DEFAULT_CHUNK_SIZE: int  # in KB
+
+    MONGODB_URL: str
+    MONGODB_DATABASE: str
 
     class Config:
         env_file = ".env"
