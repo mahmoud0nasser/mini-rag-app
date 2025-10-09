e748c2a1dc5384d8020ff652ad927960b9f2d696
mahmoud0nasser <mhnasser00@gmail.com>
2025-10-09 10:27:54 +0300

    first commit

---
+++ src/helpers/config.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    APP_NAME: str
+    APP_VERSION: str
+    OPENAI_API_KEY: str
+    FILE_ALLOWED_TYPES: list
+    FILE_MAX_SIZE: int  # in MB
+    FILE_DEFAULT_CHUNK_SIZE: int  # in KB
+
+    class Config:
+        env_file = ".env"
+
+def get_settings():
+    return Settings()
