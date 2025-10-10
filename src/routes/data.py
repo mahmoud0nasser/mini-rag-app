b4244e6b94e9efb1ad91b47a1fa739a7558e5951
mahmoud0nasser <mhnasser00@gmail.com>
2025-10-10 12:30:37 +0300

    Use async create_instance for ProjectModel and ChunkModel initialization

--- src/routes/data.py
+++ src/routes/data.py
@@ -22,7 +22,7 @@ data_router = APIRouter(
 async def upload_file(request: Request, project_id: str, file: UploadFile, 
                       app_settings: Settings=Depends(get_settings)):   
 
-    project_model = ProjectModel(
+    project_model = await ProjectModel.create_instance(
         db_client=request.app.db_client
         )
     
@@ -83,7 +83,7 @@ async def process_file(request: Request, project_id: str, process_request: Proce
     overlap_size = process_request.overlap_size
     do_reset = process_request.do_reset
 
-    project_model = ProjectModel(
+    project_model = await ProjectModel.create_instance(
         db_client=request.app.db_client
         )
     
@@ -121,7 +121,7 @@ async def process_file(request: Request, project_id: str, process_request: Proce
         for i, chunk in enumerate(file_chunks)
     ]
 
-    chunk_model = ChunkModel(
+    chunk_model = await ChunkModel.create_instance(
         db_client=request.app.db_client
     )
 
