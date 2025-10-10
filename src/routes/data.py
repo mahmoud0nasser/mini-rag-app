aa15a4a587cb34030611cf99c9ec4d0f4d5e5979
mahmoud0nasser <mhnasser00@gmail.com>
2025-10-10 15:25:12 +0300

    add asset creation and database storage during file upload process

--- src/routes/data.py
+++ src/routes/data.py
@@ -5,8 +5,10 @@ from controllers import DataController, ProjectController, ProcessController
 from .schemas.data import ProcessRequest
 from models import ResponseSignal
 from models.ProjectModel import ProjectModel
-from models.db_schemas import DataChunk
+from models.db_schemas import DataChunk, Asset
 from models.ChunkModel import ChunkModel
+from models.AssetModel import AssetModel
+from models import AssetTypeEnum
 import os
 import aiofiles
 import logging
@@ -66,12 +68,27 @@ async def upload_file(request: Request, project_id: str, file: UploadFile,
                 "detail": str(e)
             }
         )
+    
+    # Store the assets into the database
+
+    asset_model = await AssetModel.create_instance(
+        db_client=request.app.db_client
+    )
+
+    asset_resource = Asset(
+        asset_project_id=project.id,
+        asset_type=AssetTypeEnum.FILE.value,
+        asset_name=file_id,
+        asset_size=os.path.getsize(file_path)
+    )
+
+    asset_record = await asset_model.create_asset(asset=asset_resource)
 
     return JSONResponse(
         content={
             "status": "success",
             "signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value,
-            "file_id": file_id
+            "file_id": str(asset_record.id)
         }
     )
     
