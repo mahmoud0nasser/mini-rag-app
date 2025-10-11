050eb4eed24bf137b9ecd6b2e3af3d954d995c59
mahmoud0nasser <mhnasser00@gmail.com>
2025-10-11 10:19:08 +0300

    add chunk_asset_id field to DataChunk model for asset reference tracking

--- src/models/db_schemas/data_chunk.py
+++ src/models/db_schemas/data_chunk.py
@@ -8,6 +8,7 @@ class DataChunk(BaseModel):
     chunk_metadata: dict
     chunk_order: int = Field(..., gt=0)
     chunk_project_id: ObjectId
+    chunk_asset_id: ObjectId  # Reference to Asset if applicable
 
     class Config:
         arbitrary_types_allowed = True
