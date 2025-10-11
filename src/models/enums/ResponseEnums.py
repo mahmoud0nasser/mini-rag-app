e3f6dc51e26fa56167b2b9a1dd46b0af74803da5
mahmoud0nasser <mhnasser00@gmail.com>
2025-10-11 10:19:39 +0300

    add new response signals for missing or invalid file handling

--- src/models/enums/ResponseEnums.py
+++ src/models/enums/ResponseEnums.py
@@ -9,3 +9,5 @@ class ResponseSignal(Enum):
     FILE_UPLOAD_FAILED = "file_upload_failed"
     PROCESSING_SUCCESS = "processing_success"
     PROCESSING_FAILED = "processing_failed"
+    NO_FILES_TO_PROCESS = "no_found_files"
+    FILE_ID_ERROR = "no_file_found_with_id"
