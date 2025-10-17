e4c79053296b74e892b27f3dc2bfd4c949b8ec08
mahmoud0nasser <mhnasser00@gmail.com>
2025-10-17 18:22:02 +0300

    integrate LLMProviderFactory with FastAPI lifecycle and initialize generation/embedding clients during startup

--- src/main.py
+++ src/main.py
@@ -2,10 +2,11 @@ from fastapi import FastAPI
 from routes import base, data
 from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings
+from stores.llm.LLMProviderFactory import LLMProviderFactory
 
 app = FastAPI()
 
-@app.on_event("startup")
+# @app.on_event("startup") # Depricated
 async def startup_db_client():
     settings = get_settings()
     
@@ -13,10 +14,24 @@ async def startup_db_client():
     app.db_client = app.mongo_connection[settings.MONGODB_DATABASE]
     print("Connected to the MongoDB database!")
 
-@app.on_event("shutdown")
+    llm_provider_factory = LLMProviderFactory(settings)
+
+    # Generation Client
+    app.generation_client = llm_provider_factory.create(provider=settings.GENERATION_BACKEND)
+    app.generation_client.set_generation_model(model_id=settings.GENERATION_MODEL_ID)
+
+    # Embedding Client
+    app.embedding_client = llm_provider_factory.create(provider=settings.EMBEDDING_BACKEND)
+    app.embedding_client.set_embedding_model(model_id=settings.EMBEDDING_MODEL_ID,
+                                             embedding_size=settings.EMBEDDING_MODEL_SIZE)
+
+# @app.on_event("shutdown") # Depricated
 async def shutdown_db_client():
     app.mongo_connection.close()
     print("MongoDB connection closed.")
+
+app.router.lifespan.on_startup.append(startup_db_client)
+app.router.lifespan.on_shutdown.append(shutdown_db_client)
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
