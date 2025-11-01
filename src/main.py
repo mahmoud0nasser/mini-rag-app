85efbc41a6e83036bfbfed76eb170fc26018cd48
mahmoud0nasser <mhnasser00@gmail.com>
2025-11-01 19:55:53 +0200

    replace deprecated lifespan usage with on_event for startup and shutdown to avoid runtime error

--- src/main.py
+++ src/main.py
@@ -38,8 +38,11 @@ async def shutdown_span():
     app.mongo_connection.close()
     app.vectordb_client.disconnect()
 
-app.router.lifespan.on_startup.append(startup_span)
-app.router.lifespan.on_shutdown.append(shutdown_span)
+# app.router.lifespan.on_startup.append(startup_span)
+# app.router.lifespan.on_shutdown.append(shutdown_span)
+
+app.on_event("startup")(startup_span)
+app.on_event("shutdown")(shutdown_span)
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
