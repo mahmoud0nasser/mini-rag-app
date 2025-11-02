7508f59c2daa5ac6dbba576ce804c6ffe6c8c6e3
mahmoud0nasser <mhnasser00@gmail.com>
2025-11-02 21:08:24 +0200

    initialize TemplateParser during startup with language settings

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings
 from stores.llm.LLMProviderFactory import LLMProviderFactory
 from stores.vectordb.VectorDBProviderFactory import VectorDBProviderFactory
+from stores.llm.templates.template_parser import TemplateParser
 
 app = FastAPI()
 
@@ -32,6 +33,11 @@ async def startup_span():
         provider=settings.VECTOR_DB_BACKEND
         )
     app.vectordb_client.connect()
+
+    app.template_parser = TemplateParser(
+        language=settings.PRIMARY_LANG,
+        default_language=settings.DEFAULT_LANG,
+    )
 
 # @app.on_event("shutdown") # Depricated
 async def shutdown_span():
